8f848bc974810394ee06ae81f2284593fccfb300
aongwen <41949489+aongwen@users.noreply.github.com>
2020-10-28 11:33:58 +0300

    Update create_sonarqube.sh
    updated namespace

--- create_sonarqube.sh
+++ create_sonarqube.sh
@@ -15,4 +15,4 @@ ansible-playbook ./playbooks/sonarqube.yaml \
   -e "sonarqube_ssl=True" \
   -e "sonarqube_image_tag=7.9.1" \
   -e "sonarqube_route=sonarqube.apps.shared-dev.dev.openshift.opentlc.com" \
-  -e '{"meta": {"namespace":"xyz-sonarqube","name":"sonarqube"}}'
+  -e '{"meta": {"namespace":"kcb-devops-framework","name":"sonarqube"}}'
